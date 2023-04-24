22762fc7be1fff277298c7452bcf7e81f129152e
claus <duozhou@ieee.org>
2023-04-24 12:36:40 -0500

    add clear log bash

--- Code/clear_log.sh
+++ Code/clear_log.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # Prompt user for confirmation
-echo "Are you sure you want to delete all files in the current directory?"
+echo "Are you sure you want to delete all files in the ./log directory?"
 read -p "Type 'y' to confirm: " confirm
 
 # Check user confirmation
 if [ "$confirm" == "y" ]; then
-  # Remove all files in current directory
-  rm -f *
+  # Remove all files in ./log directory
+  rm -f ./log/*
 
   # Output success message
-  echo "All files in current directory have been deleted."
+  echo "All files in ./log directory have been deleted."
 else
   # Output cancel message
   echo "Operation canceled."
 fi
+
